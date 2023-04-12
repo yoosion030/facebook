2248826dcd4c9545b5e59ae89aa8656a334856e2
yoosion030 <yoosioff@gmail.com>
2023-04-12 17:54:34 +0900

    Add Header Component

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,13 @@
+import * as I from 'assets';
+import * as S from './style';
+
+const Header = () => {
+  return (
+    <S.Header>
+      <I.Logo />
+      <I.ProfileIcon />
+    </S.Header>
+  );
+};
+
+export default Header;
---
+++ src/components/Header/style.ts
@@ -0,0 +1,12 @@
+import styled from '@emotion/styled';
+
+export const Header = styled.header`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  width: 100vw;
+  padding: 0 16px;
+  height: 56px;
+  border-bottom: 1px solid #ccc;
+  background-color: #ffffff;
+`;
