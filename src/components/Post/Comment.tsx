5a8c82dc7cc0783c6146fd8f7b5035aff622c5cb
yoosion030 <yoosioff@gmail.com>
2023-04-13 09:17:52 +0900

    Update Comment textarea
    input을 textarea로 변경하여 내용 길이마다 높이를 조절할 수 있도록 개선

--- src/components/Post/Comment.tsx
+++ src/components/Post/Comment.tsx
@@ -1,11 +1,39 @@
 import styled from '@emotion/styled';
 import * as I from 'assets';
+import { useRef } from 'react';
 
 const Comment = () => {
+  const textarea = useRef<HTMLTextAreaElement>(null);
+  const handleResizeHeight = () => {
+    if (textarea.current) {
+      textarea.current.style.height = textarea.current.scrollHeight + 'px';
+    }
+  };
+
+  const handleComment = (event: React.KeyboardEvent<HTMLTextAreaElement>) => {
+    event.preventDefault();
+    handleResizeHeight();
+  };
+
+  const handleKeyDown = (event: React.KeyboardEvent<HTMLTextAreaElement>) => {
+    if (event.key === 'Enter' && !event.shiftKey) {
+      event.preventDefault();
+      handleComment(event);
+    }
+  };
+
   return (
     <CommentLayout>
       <I.ProfileIcon width={32} height={32} />
-      <CommentInput placeholder='댓글을 입력하세요...' />
+      <CommentForm>
+        <CommentInput
+          placeholder='댓글을 입력하세요...'
+          rows={1}
+          onChange={handleResizeHeight}
+          onKeyDown={handleKeyDown}
+          ref={textarea}
+        />
+      </CommentForm>
     </CommentLayout>
   );
 };
@@ -19,10 +47,16 @@ const CommentLayout = styled.div`
   border-top: 1px solid #cccccc;
 `;
 
-const CommentInput = styled.input`
+const CommentForm = styled.form`
   width: 100%;
-  padding: 0 8px;
+`;
+
+const CommentInput = styled.textarea`
+  width: 100%;
+  height: 32px;
+  padding: 8px 12px;
   background-color: #f0f2f5;
   border: none;
   border-radius: 12px;
+  resize: none;
 `;
