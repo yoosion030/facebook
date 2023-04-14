73aab34c9e75b455864865d74d99e24be5809693
yoosion030 <yoosioff@gmail.com>
2023-04-14 11:52:23 +0900

    Update Comment Style Component
    스타일과 컴포넌트 코드 분리

--- src/components/Post/Comment/Reply/ReplyList.tsx
+++ src/components/Post/Comment/Reply/ReplyList.tsx
@@ -1,36 +1,19 @@
-import styled from '@emotion/styled';
 import * as I from 'assets';
+import * as S from './style';
 import ReplyInput from './ReplyInput';
 
 const ReplyList = ({ commentId, replies }: any) => {
   return (
-    <ReplyLayout>
+    <S.ReplyLayout>
       {replies.map((reply: any) => (
-        <Reply key={reply.replyId}>
+        <S.Reply key={reply.replyId}>
           <I.ProfileIcon width={24} height={24} />
-          <ReplyComment>{reply.comment}</ReplyComment>
-        </Reply>
+          <S.ReplyComment>{reply.comment}</S.ReplyComment>
+        </S.Reply>
       ))}
       <ReplyInput commentId={commentId} />
-    </ReplyLayout>
+    </S.ReplyLayout>
   );
 };
 
 export default ReplyList;
-
-const ReplyLayout = styled.div``;
-
-const Reply = styled.div`
-  display: flex;
-
-  gap: 12px;
-  height: 52px;
-  padding-left: 42px;
-`;
-
-const ReplyComment = styled.div`
-  height: 80%;
-  padding: 8px 12px;
-  background-color: #f0f2f5;
-  border-radius: 8px;
-`;
--- src/components/Post/Comment/Reply/style.ts
+++ src/components/Post/Comment/Reply/style.ts
@@ -1,5 +1,22 @@
 import styled from '@emotion/styled';
 
+export const ReplyLayout = styled.div``;
+
+export const Reply = styled.div`
+  display: flex;
+
+  gap: 12px;
+  height: 52px;
+  padding-left: 42px;
+`;
+
+export const ReplyComment = styled.div`
+  height: 80%;
+  padding: 8px 12px;
+  background-color: #f0f2f5;
+  border-radius: 8px;
+`;
+
 export const ReplyInput = styled.textarea`
   width: 100%;
   height: 32px;
