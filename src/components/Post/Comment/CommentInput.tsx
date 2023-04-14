9266082b69903fba7c20b6b9e5c85a5d5feebd1e
yoosion030 <yoosioff@gmail.com>
2023-04-14 09:14:00 +0900

    Add CommentInput Compoentn
    댓글 입력 textarea 컴포넌트로 분리

---
+++ src/components/Post/Comment/CommentInput.tsx
@@ -0,0 +1,42 @@
+import { useRef, useContext } from 'react';
+import * as S from './style';
+import { CommentContext } from 'context/CommentProvider';
+
+const CommentInput = () => {
+  const { comments, addComment } = useContext(CommentContext);
+  const textarea = useRef<HTMLTextAreaElement>(null);
+
+  const handleResizeHeight = () => {
+    if (textarea.current) {
+      textarea.current.style.height = textarea.current.scrollHeight + 'px';
+    }
+  };
+
+  const handleKeyDown = (event: React.KeyboardEvent<HTMLTextAreaElement>) => {
+    if (event.key === 'Enter' && !event.shiftKey && !event.nativeEvent.isComposing) {
+      event.preventDefault();
+      handleComment(event);
+    }
+  };
+
+  const handleComment = (event: React.KeyboardEvent<HTMLTextAreaElement>) => {
+    const comment = textarea.current?.value;
+    if (!comment) return;
+    event.preventDefault();
+    textarea.current.value = '';
+    textarea.current.style.height = '32px';
+    addComment(comment, comments.length);
+  };
+
+  return (
+    <S.CommentInput
+      placeholder='댓글을 입력하세요...'
+      rows={1}
+      onChange={handleResizeHeight}
+      onKeyDown={handleKeyDown}
+      ref={textarea}
+    />
+  );
+};
+
+export default CommentInput;
--- src/components/Post/Comment/CommentList.tsx
+++ src/components/Post/Comment/CommentList.tsx
@@ -1,50 +1,22 @@
-import { useRef, useContext } from 'react';
+import { useContext } from 'react';
 import * as I from 'assets';
 import * as S from './style';
 import Comment from './Comment';
 import { CommentContext } from 'context/CommentProvider';
+import CommentInput from './CommentInput';
 
 interface CommentProps {
   id: string;
 }
 
 const CommentList = ({ id }: CommentProps) => {
-  const { comments, addComment } = useContext(CommentContext);
-  const textarea = useRef<HTMLTextAreaElement>(null);
-
-  const handleResizeHeight = () => {
-    if (textarea.current) {
-      textarea.current.style.height = textarea.current.scrollHeight + 'px';
-    }
-  };
-
-  const handleKeyDown = (event: React.KeyboardEvent<HTMLTextAreaElement>) => {
-    if (event.key === 'Enter' && !event.shiftKey && !event.nativeEvent.isComposing) {
-      event.preventDefault();
-      handleComment(event);
-    }
-  };
-
-  const handleComment = (event: React.KeyboardEvent<HTMLTextAreaElement>) => {
-    const comment = textarea.current?.value;
-    if (!comment) return;
-    event.preventDefault();
-    textarea.current.value = '';
-    textarea.current.style.height = '32px';
-    addComment(comment, comments.length);
-  };
+  const { comments } = useContext(CommentContext);
 
   return (
     <S.CommentListLayout>
       <I.ProfileIcon width={32} height={32} />
       <S.CommentForm>
-        <S.CommentInput
-          placeholder='댓글을 입력하세요...'
-          rows={1}
-          onChange={handleResizeHeight}
-          onKeyDown={handleKeyDown}
-          ref={textarea}
-        />
+        <CommentInput />
 
         {comments.map(comment => (
           <Comment key={comment.commentId} {...comment} />
