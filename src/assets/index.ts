a54bc2292a8883a6faccfe58a077a3710ac302d9
yoosion030 <yoosioff@gmail.com>
2023-04-19 08:46:33 +0900

    Delete LikeCountIcon

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -3,4 +3,3 @@ export { default as ProfileIcon } from './common/ProfileIcon';
 export { default as LikeIcon } from './SocialMediaButton/LikeIcon';
 export { default as CommentIcon } from './SocialMediaButton/CommentIcon';
 export { default as BlueLikeIcon } from './SocialMediaButton/BlueLikeIcon';
-export { default as LikeCountIcon } from './LikeCommentCount/LikeCountIcon';
